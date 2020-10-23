c4bebd48c99e93cb15e20e9fe4642e1790fd353a
Koramit Pichanaharee <koramit.p@gmail.com>
2020-10-23 12:31:59 +0700

    init user pages

---
+++ resources/js/Pages/Users/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+    <h1>Dashboard</h1>
+    <inertia-link method="post" :href="`${$page.props.app.baseUrl}/logout`">logout</inertia-link>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
