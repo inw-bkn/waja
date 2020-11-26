dc6533003a1b7065ff788adcbe503700fe590b2c
Koramit Pichanaharee <koramit.p@gmail.com>
2020-11-26 09:35:42 +0700

    upgrade tailwindcss => 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-    defaultLineHeights: true,
-    standardFontWeights: true
-  },
-  purge: ['./resources/views/**/*.blade.php', './resources/js/**/*.vue'],
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+        defaultLineHeights: true,
+        standardFontWeights: true
+    },
+    purge: ['./resources/views/**/*.blade.php', './resources/js/**/*.vue'],
+    theme: {
+    extend: {
+        colors: {
+            teal: colors.teal
+        }
+    }
+    },
+    variants: {},
+    plugins: []
 }
